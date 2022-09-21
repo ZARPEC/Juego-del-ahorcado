1a89157fbe6771a885ff9588ae9b9f5832197232
ZARPEC <107159580+ZARPEC@users.noreply.github.com>
2022-09-20 21:38:41 -0600

    avance codigo js

--- js/dibujo.js
+++ js/dibujo.js
@@ -2,14 +2,16 @@ var dibujo = document.getElementById("areaDibujo");
 var puntero = dibujo.getContext("2d");
 
 puntero.fillStyle = "white";
-puntero.fillRect(0, 0, 500, 500);
+puntero.fillRect(0, 0, 500, 450);
 
 
 var letras = document.getElementById("areaLetras");
 var punteroL = letras.getContext("2d");
 
-punteroL.fillStyle = "black"
+punteroL.fillStyle = "white"
 punteroL.fillRect(0, 0, 500, 100)
+
+var cantidad;
 
 function crearBase(x, y, a, l) {
     puntero.fillStyle = "#0A3871";
@@ -17,14 +19,63 @@ function crearBase(x, y, a, l) {
 
 }
 
+
+
 function Palabra() {
+    var palabras = ["laptop", "celular", "cuaderno"];
+
+    var selec = palabras[Math.floor(Math.random() * palabras.length)];
+
+    var cantidad = selec.length;
+
+
+    var x = 10;
+
+    for (var numero = 0; numero < cantidad; numero++) {
+        var letra = selec.charAt(numero);
+        DibLinea(x);
+        DibLetra(x + 25, letra)
+        x += 65;
+        console.log(letra);
+    }
+
+    console.log(cantidad);
+    console.log(selec);
 
 
 }
 
+function DibLinea(x) {
+
+    punteroL.fillStyle = "#0A3871";
+
+    punteroL.fillRect(x, 50, 50, 3);
+}
+
+function DibLetra(x, letra) {
+
+    punteroL.textAlign = "center";
+    punteroL.font = "30pt arial"
+    punteroL.fillStyle = "black"
+    punteroL.lineWidth = 4;
+    punteroL.strokeText(letra, x, 45);
+}
+
+
+
+
+//barra vertical
 crearBase(50, 50, 3, 400);
-crearBase(50, 50, 400, 3)
-crearBase(0, 450, 450, 3)
+//arriba
+crearBase(50, 50, 400, 3);
+
+//base
+crearBase(0, 450, 450, 3);
+
+Palabra();
+
+
+
 
 
 //214+35
